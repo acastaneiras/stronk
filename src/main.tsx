236bea752eff0b47b9076ba3f99ce6c729cc4009
Alex Castañeiras Bueno <33935297+acastaneiras@users.noreply.github.com>
2024-11-13 20:30:05 +0100

    feat: Add sonner & theme functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/index.css'
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from '@/components/ui/sonner';
+import '@/index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
 import router from './router/router';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
   </StrictMode>,
 )
