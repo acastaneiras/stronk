89998a3af8e4fe2a68f2720abb02c9388d1321c2
acastaneiras <acastaneiras@users.noreply.github.com>
2024-12-20 16:26:47 +0000

    deploy: 067b0ad0e8670956442e43debb522cf4cac7c6c3

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/stronk/sw.js', { scope: '/stronk/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
